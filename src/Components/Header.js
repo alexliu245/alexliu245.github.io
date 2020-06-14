b742533f1cba03cfe767088e488d888ceaec72d6
Alex Liu <alex.liu245@gmail.com>
2020-06-13 21:56:12 -0400

    updated header copy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -32,8 +32,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a <span>{occupation}</span>.</h3>
+            <h1 className="responsive-headline">Hi! I'm {name}.</h1>
+            <h3>I'm a <span>{occupation}</span>. {description}</h3>
             <hr />
             <ul className="social">
                {networks}
