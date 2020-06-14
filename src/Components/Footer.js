c0243e4d930431d61eec329982d6d8a66b83209e
Alex Liu <alex.liu245@gmail.com>
2020-06-13 21:42:46 -0400

    updated copright for header background

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,6 +21,7 @@ class Footer extends Component {
            <ul className="copyright">
               <li>&copy; Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li><a href="https://www.vecteezy.com/free-vector/web">Web Vectors by Vecteezy</a></li>
            </ul>
 
         </div>
